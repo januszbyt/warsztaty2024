15f907a1840f963ef27f3266bcd4c813c4a7ceac
matsz <matsz@172.28.20.147>
2024-04-14 16:20:13 +0200

    Dodanie otwierdania folderu i automatyczne wyswietlanie zdjecia.

--- src/main/java/com/mycompany/warsztaty2024/SzczegolController.java
+++ src/main/java/com/mycompany/warsztaty2024/SzczegolController.java
@@ -13,7 +13,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -38,7 +41,7 @@ public class SzczegolController implements Initializable {
     @FXML private TextField statusszcz;
     @FXML private TextField linkszcz;
     @FXML private TextField zrodloszcz;
-          
+    @FXML private ImageView myimage; 
          
     public static int pracownik_id;
 
@@ -57,9 +60,93 @@ private void switchToKarta_Kierowcy() throws IOException {
     newWindow.initModality(Modality.APPLICATION_MODAL);
     newWindow.showAndWait();
     }
+
+
+@FXML
+private void openPersonalFolder() {
+    String mainFolderPath = "C:\\skany\\";
+    String employeeFolderName = idszcz.getText(); // Pobranie ID pracownika jako nazwa folderu
+    String employeeFolderPath = mainFolderPath + employeeFolderName;
+    
+    System.out.println("Próba otwarcia folderu pracownika: " + employeeFolderPath); // Wydrukowanie ścieżki folderu pracownika
+    
+    File employeeFolder = new File(employeeFolderPath);
+    
+    if (employeeFolder.exists() && employeeFolder.isDirectory()) {
+        try {
+            if (System.getProperty("os.name").toLowerCase().contains("win")) {
+                // Dla systemów Windows
+                Runtime.getRuntime().exec("explorer.exe " + employeeFolder.getAbsolutePath());
+            } else if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+                // Dla systemów macOS
+                Runtime.getRuntime().exec("open " + employeeFolder.getAbsolutePath());
+            } else {
+                // Dla systemów Linux
+                Runtime.getRuntime().exec("xdg-open " + employeeFolder.getAbsolutePath());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            showAlert("Błąd", "Nie można otworzyć folderu.", "Sprawdź, czy masz odpowiednie uprawnienia i czy ścieżka jest prawidłowa.");
+        }
+    } else {
+        showAlert("Błąd", "Folder nie istnieje", "Upewnij się, że ścieżka jest prawidłowa i folder istnieje.");
+    }
+}
+
+
+
+private void loadEmployeePhoto() {
+    String mainFolderPath = "C:\\skany\\";
+    String employeeFolderName = idszcz.getText(); // Pobranie ID pracownika jako nazwa folderu
+    if (employeeFolderName == null || employeeFolderName.trim().isEmpty()) {
+        showAlert("Błąd", "Brak ID pracownika", "Nie można wczytać zdjęcia, brak ID pracownika.");
+        return;
+    }
+    
+    String employeeFolderPath = mainFolderPath + employeeFolderName + "\\zdjecie\\";
+    
+    System.out.println("Próba wczytania zdjęcia pracownika z folderu: " + employeeFolderPath); // Wydrukowanie ścieżki folderu pracownika
+    
+    File employeeFolder = new File(employeeFolderPath);
+
+    if (employeeFolder.exists() && employeeFolder.isDirectory()) {
+        try {
+            // Wydrukuj zawartość folderu dla celów diagnostycznych
+            System.out.println("Zawartość folderu:");
+            for (File file : employeeFolder.listFiles()) {
+                System.out.println(file.getName());
+            }
+
+            File[] files = employeeFolder.listFiles((dir, name) -> name.toLowerCase().endsWith(".jpg") || name.toLowerCase().endsWith(".png"));
+            
+            if (files != null && files.length > 0) {
+                File photoFile = new File(employeeFolderPath + files[0].getName()); 
+                Image image = new Image(photoFile.toURI().toString());
+                myimage.setImage(image);
+            } else {
+                showAlert("Błąd", "Brak zdjęcia", "W folderze pracownika nie ma zdjęcia.");
+            }
+        } catch (Exception e) {
+            showAlert("Błąd", "Błąd wczytywania", "Wystąpił błąd podczas wczytywania zdjęcia.");
+            e.printStackTrace();
+        }
+    } else {
+        showAlert("Błąd", "Folder nie istnieje", "Folder pracownika nie istnieje.");
+    }
+}
+
+private void showAlert(String title, String headerText, String contentText) {
+    Alert alert = new Alert(Alert.AlertType.ERROR);
+    alert.setTitle(title);
+    alert.setHeaderText(headerText);
+    alert.setContentText(contentText);
+    alert.showAndWait();
+}
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
         nazwiskoszcz.setText(PrimaryController.wybranaOsobaDalej.getNazwisko());
         idszcz.setText(Integer.toString(PrimaryController.wybranaOsobaDalej.getID()));
         imieszcz.setText(PrimaryController.wybranaOsobaDalej.getImie());
@@ -70,5 +157,6 @@ private void switchToKarta_Kierowcy() throws IOException {
         zrodloszcz.setText(PrimaryController.wybranaOsobaDalej.getZrodlo());
         statusszcz.setText(PrimaryController.wybranaOsobaDalej.getStatus());
         linkszcz.setText(PrimaryController.wybranaOsobaDalej.getLink());
+         loadEmployeePhoto();
     }
     }
