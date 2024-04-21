3397a13447d58240b9de12b3d926f8bd31a660df
kojde <kojde@RogStrix_Home>
2024-04-21 20:49:39 +0200

    Dołączono aktualny plik bazy danych dodano monit o praku połaczenia z bazą podczas logowania

--- src/main/java/com/mycompany/warsztaty2024/LoginController.java
+++ src/main/java/com/mycompany/warsztaty2024/LoginController.java
@@ -1,16 +1,16 @@
 package com.mycompany.warsztaty2024;
 
-import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class 
-        LoginController {
+public class LoginController {
 
     @FXML
     private TextField txtUsername;
@@ -23,23 +23,30 @@ public class
         String username = txtUsername.getText();
         String password = txtPassword.getText();
 
-        try (Connection conn = DatabaseConnection.getConnection();
-             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users WHERE username=? AND password=?")) {
-
+        try {
+            Connection conn = DatabaseConnection.getConnection();
+            // Sprawdzanie poprawności danych logowania
+            String query = "SELECT * FROM users WHERE username=? AND password=?";
+            PreparedStatement stmt = conn.prepareStatement(query);
             stmt.setString(1, username);
             stmt.setString(2, password);
-
             ResultSet rs = stmt.executeQuery();
 
             if (rs.next()) {
+                // Jeśli znaleziono pasujące dane, kontynuuj logowanie
+                showAlert(Alert.AlertType.INFORMATION, "Sukces", "Logowanie udane", "Witaj, " + username + "!");
                 App.setMainScene();
             } else {
-               
+                // Jeśli nie znaleziono pasujących danych, wyświetl alert o nieprawidłowym loginie lub haśle
                 showAlert(Alert.AlertType.ERROR, "Błąd logowania", "Nieprawidłowy login lub hasło", "Spróbuj ponownie.");
             }
-
         } catch (SQLException e) {
-            e.printStackTrace();
+            // Jeśli wystąpi błąd podczas łączenia z bazą danych, wyświetl alert
+            showAlert(Alert.AlertType.ERROR, "Błąd logowania", "Błąd połączenia z bazą danych", "Sprawdź połączenie z internetem oraz ustawienia bazy danych.");
+
+            // W przypadku błędu połączenia z bazą danych, wypisz informację o błędzie
+            System.err.println("Błąd połączenia z bazą danych: " + e.getMessage());
+        
         }
     }
 
